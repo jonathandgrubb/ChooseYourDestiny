a96c721ae0458da3dbca9a8abe93edf596e07ac9
Jonathan Grubb <geekygamer@gmail.com>
2016-09-05 17:48:30 -0500

    some layout for Manage Story view. fixed Library view to display downloaded vs. non downloaded titles. 

--- ChooseYourDestiny/ChooseYourDestiny/StoriesViewController.swift
+++ ChooseYourDestiny/ChooseYourDestiny/StoriesViewController.swift
@@ -52,24 +52,31 @@ class StoriesViewController: UIViewController, UITableViewDelegate, UITableViewD
         cell.detailTextLabel?.numberOfLines = 0
         cell.detailTextLabel?.lineBreakMode = NSLineBreakMode.ByWordWrapping
         
-        switch indexPath.row {
-        case 0:
-            cell.textLabel?.text = "High Seas"
-            cell.detailTextLabel?.text = "Not Started (200 chapters)"
-        case 1:
-            cell.textLabel?.text = "Adventure!"
-            cell.detailTextLabel?.text = "Not Started (231 chapters)"
-        case 2:
-            cell.textLabel?.text = "Meh - the Novel"
-            cell.detailTextLabel?.text = "Started (109 chapters)"
-        case 3:
-            cell.textLabel?.text = "High Seas II"
-            cell.detailTextLabel?.text = "Not On Device (109 chapters)"
-        case 4:
-            cell.textLabel?.text = "The Octopus"
-            cell.detailTextLabel?.text = "Not On Device (351 chapters)"
-        default:
-            cell.textLabel?.text = "Undefined"
+        if (indexPath.section == 0) {
+            switch indexPath.row {
+            case 0:
+                cell.textLabel?.text = "High Seas"
+                cell.detailTextLabel?.text = "Started (200 chapters)"
+            case 1:
+                cell.textLabel?.text = "Adventure!"
+                cell.detailTextLabel?.text = "Not Started (231 chapters)"
+            case 2:
+                cell.textLabel?.text = "Meh - the Novel"
+                cell.detailTextLabel?.text = "Started (109 chapters)"
+            default:
+                cell.textLabel?.text = "Undefined"
+            }
+        } else {
+            switch indexPath.row {
+            case 0:
+                cell.textLabel?.text = "High Seas II"
+                cell.detailTextLabel?.text = "Not On Device (109 chapters)"
+            case 1:
+                cell.textLabel?.text = "The Octopus"
+                cell.detailTextLabel?.text = "Not On Device (351 chapters)"
+            default:
+                cell.textLabel?.text = "Undefined"
+            }
         }
         
         return cell;
