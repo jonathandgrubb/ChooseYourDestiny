e543d5f38ff5ea7d94dde369779f995adb86ab2d
Jonathan Grubb <geekygamer@gmail.com>
2016-10-15 11:41:01 -0500

    reading view: flexible cell height for choices, but tableview is still a static height

--- ChooseYourDestiny/ChooseYourDestiny/ReadingViewController.swift
+++ ChooseYourDestiny/ChooseYourDestiny/ReadingViewController.swift
@@ -16,32 +16,38 @@ class ReadingViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 140
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return 10
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         // Create the cell
-        let cell = tableView.dequeueReusableCellWithIdentifier("ChoiceCell", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("ChoiceCell", forIndexPath: indexPath) as! ChoiceCell
         
         // http://stackoverflow.com/a/1754259
         cell.detailTextLabel?.numberOfLines = 0
-        cell.detailTextLabel?.lineBreakMode = NSLineBreakMode.ByWordWrapping
+        //cell.detailTextLabel?.lineBreakMode = NSLineBreakMode.ByWordWrapping
         
         switch indexPath.row {
         case 0:
-            cell.textLabel?.text = "Welcome"
-            cell.detailTextLabel?.text = "Welcome back to the Choose Your Destiny app, an open source project fueled by stories that can be experienced and changed by you! See http://www.blahblah.com for details"
+            cell.textView.text = "Grab one of the blahs!!! Grab one of the blahs!!! Grab one of the blahs!!! Grab one of the blahs!!! Grab one of the blahs!!! Grab one of the blahs!!! Grab one of the blahs!!! Grab one of the blahs!!! Grab one of the blahs!!! Grab one of the blahs!!! Grab one of the blahs!!! Grab one of the blahs!!!"
+            //cell.textLabel?.text = "Welcome"
+            //cell.detailTextLabel?.text = "Welcome back to the Choose Your Destiny app, an open source project fueled by stories that can be experienced and changed by you! See http://www.blahblah.com for details"
         case 1:
-            cell.textLabel?.text = "Continue Reading..."
-            cell.detailTextLabel?.text = "High Seas"
+            cell.textView.text = "Get that other blah!!!"
+            //cell.textLabel?.text = "Continue Reading..."
+            //cell.detailTextLabel?.text = "High Seas"
         case 2:
-            cell.textLabel?.text = "Find a new story"
+            cell.textView.text = "Don't open that door!!!"
+            //cell.textLabel?.text = "Find a new story"
         default:
-            cell.textLabel?.text = "Find a new story"
+            cell.textView.text = "Run back out the door screaming!!!"
+            //cell.textLabel?.text = "Find a new story"
         }
         
         return cell;
