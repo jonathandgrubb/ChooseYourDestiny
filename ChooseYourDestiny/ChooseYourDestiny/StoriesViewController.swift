7a6d17466668e9d81508bb5697735c86e530101f
Jonathan Grubb <geekygamer@gmail.com>
2016-10-16 18:06:45 -0500

    attempts to get Chapter fetches working, but probably won't work until we can get a story to save to CoreData

--- ChooseYourDestiny/ChooseYourDestiny/StoriesViewController.swift
+++ ChooseYourDestiny/ChooseYourDestiny/StoriesViewController.swift
@@ -192,16 +192,16 @@ class StoriesViewController: UIViewController, UITableViewDelegate, UITableViewD
             print("read a story")
             
             // create fetch request for the selected story
-            if let fc = fetchedResultsController, let story = fc.objectAtIndexPath(indexPath) as? Story {
+            if let story = fetchedResultsController!.objectAtIndexPath(indexPath) as? Story {
 
                 let fr = NSFetchRequest(entityName: "Chapter")
                 fr.sortDescriptors = []
-                let pred = NSPredicate(format: "(is_first_chapter = \"true\") AND (story = %@)", story)
+                let pred = NSPredicate(format: "(is_first_chapter = %@) AND (story = %@)", "true", story)
                 fr.predicate = pred
                 executeSearch()
                 
                 // go to the reading tab (and start at the beginning of the story)
-                if let chapters = fc.fetchedObjects as? [Chapter] { // where chapters.count == 1 {
+                if let chapters = fetchedResultsController!.fetchedObjects as? [Chapter] where chapters.count == 1 {
                     GitHubClient.sharedInstance().startAtBeginning = true
                     GitHubClient.sharedInstance().currentChapter = chapters[0]
                     self.tabBarController?.selectedIndex = 1
